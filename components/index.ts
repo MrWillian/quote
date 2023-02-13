9c832d079bf2d6ed8896e1dc7ebb542c47c794bb
MrWillian <willian.dev10@gmail.com>
2023-02-13 11:49:50 -0300

    refactor(components): add index to export components

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import { LoginButton } from './LoginButton';
+import { RegisterButton } from './RegisterButton';
+import { SocialLoginButton } from './SocialLoginButton';
+import { WriteDownContainer } from './WriteDownContainer';
+import Layout from './Layout';
+import AuthLayout from './AuthLayout';
+
+export {
+    LoginButton, RegisterButton, SocialLoginButton, WriteDownContainer, Layout, AuthLayout
+};
