aea1913f479f1dd90a3be07452c905272d847530
MrWillian <willian.dev10@gmail.com>
2023-02-13 12:04:41 -0300

    refactor(components): export QuoteAppIcon

--- components/index.ts
+++ components/index.ts
@@ -3,9 +3,10 @@ import { RegisterButton } from './RegisterButton';
 import { SocialLoginButton } from './SocialLoginButton';
 import { WriteDownContainer } from './WriteDownContainer';
 import { FormHeader } from './FormHeader';
+import { QuoteAppIcon } from './QuoteAppIcon';
 import Layout from './Layout';
 import AuthLayout from './AuthLayout';
 
 export {
-    LoginButton, RegisterButton, SocialLoginButton, WriteDownContainer, FormHeader, Layout, AuthLayout
+    LoginButton, RegisterButton, SocialLoginButton, WriteDownContainer, FormHeader, QuoteAppIcon, Layout, AuthLayout
 };
