ad2d708a82bb7218df5767ace9eeaaad95dcc5e1
MrWillian <willian.dev10@gmail.com>
2023-02-13 11:59:26 -0300

    refactor(components): export FormHeader

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,10 @@ import { LoginButton } from './LoginButton';
 import { RegisterButton } from './RegisterButton';
 import { SocialLoginButton } from './SocialLoginButton';
 import { WriteDownContainer } from './WriteDownContainer';
+import { FormHeader } from './FormHeader';
 import Layout from './Layout';
 import AuthLayout from './AuthLayout';
 
 export {
-    LoginButton, RegisterButton, SocialLoginButton, WriteDownContainer, Layout, AuthLayout
+    LoginButton, RegisterButton, SocialLoginButton, WriteDownContainer, FormHeader, Layout, AuthLayout
 };
