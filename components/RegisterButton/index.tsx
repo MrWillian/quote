313c6906abfb5e9d77d42b9b2eab364f7c27f9bc
MrWillian <willian.dev10@gmail.com>
2023-02-13 10:29:30 -0300

    feat(components): create RegisterButton

---
+++ components/RegisterButton/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const RegisterButton = () => (
+  <button className="flex justify-center items-center w-full py-2 px-4 bg-accent-color rounded shadow-lg">
+    <span className="text-xl tracking-widest hover:tracking-tight uppercase font-semibold">Registrar</span>
+  </button>
+);
