e809f1b0bd907cde92d0d73eae4fc0e80703d43b
MrWillian <willian.dev10@gmail.com>
2023-02-13 12:14:39 -0300

    refactor(components): export PasswordInput

--- components/index.ts
+++ components/index.ts
@@ -5,9 +5,10 @@ import { WriteDownContainer } from './WriteDownContainer';
 import { FormHeader } from './FormHeader';
 import { QuoteAppIcon } from './QuoteAppIcon';
 import { EmailInput } from './EmailInput';
+import { PasswordInput } from './PasswordInput';
 import Layout from './Layout';
 import AuthLayout from './AuthLayout';
 
 export {
-    LoginButton, RegisterButton, SocialLoginButton, WriteDownContainer, FormHeader, QuoteAppIcon, EmailInput, Layout, AuthLayout
+    LoginButton, RegisterButton, SocialLoginButton, WriteDownContainer, FormHeader, QuoteAppIcon, EmailInput, PasswordInput, Layout, AuthLayout
 };
