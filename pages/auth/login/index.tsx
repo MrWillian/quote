e51b3d29c8b7363e27a45eb214b3a8b181fe62c6
MrWillian <willian.dev10@gmail.com>
2023-02-13 10:45:16 -0300

    refactor(screen): center Logo

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -17,7 +17,7 @@ const Login = () => {
             <div className="flex items-center justify-center py-4 px-2 h-screen">
                 <WriteDownContainer />
                 <section className='flex flex-col h-full justify-center my-4 mx-10 border-r-5 border-gray-500'>
-                    <div className='flex w-full items-start'>
+                    <div className='flex w-full justify-center items-start'>
                         <Image 
                             className='flex items-center'
                             src={icon} 
