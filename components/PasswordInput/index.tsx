4dbc14f2e234efeb13486e20c402c2796f857d08
MrWillian <willian.dev10@gmail.com>
2023-02-15 21:15:59 -0300

    feat(component): implement value props on PasswordInput

--- components/PasswordInput/index.tsx
+++ components/PasswordInput/index.tsx
@@ -1,6 +1,17 @@
-export const PasswordInput = () => (
+type Props = {
+    value?: string,
+    onChange?: React.ChangeEventHandler<HTMLInputElement>,
+}
+
+export const PasswordInput = ({ value, onChange }: Props) => (
     <div className='flex flex-col justify-center my-2'>
         <label className='text-sm font-bold' htmlFor="password">Senha</label>
-        <input type="password" name="password" className="rounded w-full px-2 py-1 shadow-lg text-black" />
+        <input 
+            type="password" 
+            name="password" 
+            value={value}
+            onChange={onChange}
+            className="rounded w-full px-2 py-1 shadow-lg text-black"
+        />
     </div>
 );
