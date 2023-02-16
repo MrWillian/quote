89f41d774f41625376f484a430d5bda817404057
MrWillian <willian.dev10@gmail.com>
2023-02-15 21:15:45 -0300

    feat(component): implement value props on EmailInput

--- components/EmailInput/index.tsx
+++ components/EmailInput/index.tsx
@@ -1,10 +1,19 @@
 type Props = {
+    value?: string,
+    onChange?: React.ChangeEventHandler<HTMLInputElement>,
     inputRef: any
 }
 
-export const EmailInput = ({ inputRef }: Props) => (
+export const EmailInput = ({ value, onChange, inputRef }: Props) => (
     <div className='flex flex-col justify-center my-2'>
         <label className='text-sm font-bold' htmlFor="email">Email</label>
-        <input type="email" name="email" ref={inputRef} className="rounded w-full px-2 py-1 shadow-lg text-black" />
+        <input 
+            type="email" 
+            name="email" 
+            value={value}
+            onChange={onChange}
+            ref={inputRef} 
+            className="rounded w-full px-2 py-1 shadow-lg text-black"
+        />
     </div>
 );
