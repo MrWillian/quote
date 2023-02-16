44cc202c02043627c5bfb8624416cd94a3c9340a
MrWillian <willian.dev10@gmail.com>
2023-02-16 12:31:01 -0300

    refactor(components): set focus

--- components/ConfirmationContainer/index.tsx
+++ components/ConfirmationContainer/index.tsx
@@ -1,24 +1,33 @@
-export const ConfirmationContainer = () => (
-    <div className="my-8">
-        <div className="flex items-center justify-between gap-4">
-            <CodeInput />
-            <CodeInput />
-            <CodeInput />
-            <CodeInput />
-            <CodeInput />
-            <CodeInput />
-        </div>
-        <div className='flex items-center justify-center gap-2'>
-            <p>Não recebeu o código?</p>
-            <a href="#" className="underline">Reenviar</a>
-        </div>
-    </div>
-);
+import { useFocus } from "../../hooks/useFocus";
+import { InputProps } from "../../interfaces/types";
 
-const CodeInput = () => (
+export const ConfirmationContainer = () => {
+    const [ inputRef ] = useFocus();
+
+    return (
+        <div className="my-8">
+            <div className="flex items-center justify-between gap-4">
+                <CodeInput inputRef={inputRef} />
+                <CodeInput />
+                <CodeInput />
+                <CodeInput />
+                <CodeInput />
+                <CodeInput />
+            </div>
+            <div className='flex items-center justify-center gap-2'>
+                <p>Não recebeu o código?</p>
+                <a href="#" className="underline">Reenviar</a>
+            </div>
+        </div>
+    );
+};
+
+
+const CodeInput = ({ inputRef }: InputProps) => (
     <input 
         type="text"
         className="text-center h-16 w-1/6 text-5xl font-black text-accent-color rounded-md shadow-md"
         maxLength={1}
+        ref={inputRef}
     />
 );
