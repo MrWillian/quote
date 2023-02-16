bd382c2fd19746fb9fde8a5b19fc89de7bc62403
MrWillian <willian.dev10@gmail.com>
2023-02-15 21:20:01 -0300

    feat(pages): implement Register with Cognito

--- pages/auth/register/index.tsx
+++ pages/auth/register/index.tsx
@@ -1,10 +1,54 @@
+import { CognitoUser, CognitoUserAttribute } from 'amazon-cognito-identity-js';
 import Link from 'next/link';
+import { useState } from 'react';
 import { FcRight } from 'react-icons/fc';
-import { AuthLayout, RegisterButton, WriteDownContainer, FormHeader, QuoteAppIcon, EmailInput, PasswordInput } from '../../../components';
+import { 
+    AuthLayout,
+    RegisterButton,
+    WriteDownContainer,
+    FormHeader,
+    QuoteAppIcon,
+    EmailInput,
+    PasswordInput
+} from '../../../components';
+import userPool from '../../../config/userPool';
 import { useFocus } from '../../../hooks/useFocus';
 
 const Register = () => {
+    const [ givenName, setGivenName ] = useState<string>('');
+    const [ email, setEmail ] = useState<string>('');
+    const [ password, setPassword ] = useState<string>('');
+    const [ confirmPassword, setConfirmPassword ] = useState<string>('');
     const [ inputRef ] = useFocus();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let givenNameAttribute: CognitoUserAttribute = new CognitoUserAttribute({
+            Name: 'given_name', Value: givenName 
+        });
+        let emailAttribute: CognitoUserAttribute = new CognitoUserAttribute({ 
+            Name: 'email', Value: email 
+        });
+
+        let cognitoUser = new CognitoUser({ Username: email, Pool: userPool });
+
+        userPool.signUp(email, password, [givenNameAttribute, emailAttribute], null, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+
+            cognitoUser.verifyAttribute("email", "", {
+                onSuccess: (data) => {
+                    console.log('onSuccess: ', data);
+                },
+                onFailure: (err) => {
+                    console.error('onFailure: ', err);
+                }
+            });
+
+            console.log(data);
+        })
+    }
 
     return (
         <AuthLayout title="Register | Quote App">
@@ -13,18 +57,44 @@ const Register = () => {
                     <QuoteAppIcon />
                     <FormHeader title="Crie sua conta!!" subtitle="Será rápido e fácil..." />
                     <div>
-                        <form>
-                            <EmailInput inputRef={inputRef} />
-                            <PasswordInput />
+                        <form onSubmit={handleSubmit}>
+                            <div className='flex flex-col justify-center my-2'>
+                                <label className='text-sm font-bold' htmlFor="given_name">Nome</label>
+                                <input 
+                                    value={givenName} 
+                                    onChange={(event) => setGivenName(event.target.value)} 
+                                    type="text" 
+                                    name="given_name" 
+                                    id="given_name" 
+                                    className="rounded w-full px-2 py-1 shadow-lg text-black" 
+                                />
+                            </div>
+                            <EmailInput 
+                                inputRef={inputRef} 
+                                value={email} 
+                                onChange={(event) => setEmail(event.target.value)} 
+                            />
+                            <PasswordInput 
+                                value={password} 
+                                onChange={(event) => setPassword(event.target.value)} 
+                            />
                             <div className='flex flex-col justify-center my-2'>
                                 <label className='text-sm font-bold' htmlFor="confirmPassword">Confirmar Senha</label>
-                                <input type="password" name="confirmPassword" id="confirmPassword" className="rounded w-full px-2 py-1 shadow-lg text-black" />
+                                <input 
+                                    value={confirmPassword} 
+                                    onChange={(event) => setConfirmPassword(event.target.value)} 
+                                    type="password" 
+                                    name="confirmPassword" 
+                                    id="confirmPassword" 
+                                    className="rounded w-full px-2 py-1 shadow-lg text-black" 
+                                />
                             </div>
-                           
                             <br />
                             <RegisterButton />
                             <Link href="/auth/login" className='flex items-center my-2'>
-                                <span className='text-sm underline font-extralight mr-1'>Já tem uma conta? Faça Login...</span>
+                                <span className='text-sm underline font-extralight mr-1'>
+                                    Já tem uma conta? Faça Login...
+                                </span>
                                 <FcRight size={'1.5em'} color={'#282A37'} />
                             </Link>
                         </form>
