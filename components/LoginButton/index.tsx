a11feb49a07aa1c9a87dfd73da1013d33c0a9c54
MrWillian <willian.dev10@gmail.com>
2023-02-15 21:16:51 -0300

    feat(component): implement submit type on LoginButton

--- components/LoginButton/index.tsx
+++ components/LoginButton/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export const LoginButton = () => (
-  <button className="flex justify-center items-center w-full py-2 px-4 bg-accent-color rounded shadow-lg">
+  <button 
+    type='submit'
+    className="flex justify-center items-center w-full py-2 px-4 bg-accent-color rounded shadow-lg"
+  >
     <span className="text-xl tracking-widest hover:tracking-tight uppercase font-semibold">Login</span>
   </button>
 );
