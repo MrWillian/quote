34d9dad49ba92fd464bc8026373a35f1c401e72a
MrWillian <willian.dev10@gmail.com>
2023-02-17 15:40:05 -0300

    feat(contexts): set CodeContext

---
+++ contexts/CodeContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState } from "react";
+import { ChildrenProps } from "../interfaces/types";
+
+type CodeConfirmationContextType = {
+    code: string[];
+    getCode: () => string;
+    handleCode: (name: string, value: string) => void;
+};
+
+const codeConfirmationContextDefaultValues: CodeConfirmationContextType = {
+    code: null,
+    getCode: () => null,
+    handleCode: () => null,
+}
+
+const CodeConfirmationContext = createContext<CodeConfirmationContextType>(codeConfirmationContextDefaultValues);
+
+export const useCodeConfirmation = () => useContext(CodeConfirmationContext);
+
+export const CodeConfirmationProvider = ({ children }: ChildrenProps) => {
+    const [ code, setCode ] = useState<string[]>([]);
+
+    const handleCode = (name: string, value: string) => {
+        setCode(prev => ({
+            ...prev, [name]: value
+        }));
+    }
+
+    const getCode = () => objToString(code);
+
+    function objToString(obj) {
+        let aux = '';
+        return Object.entries(obj).reduce((str, [p, val]) => {
+            aux += val;
+            return `${aux}`;
+        }, '');
+    }
+
+    const value = { code, getCode, handleCode };
+    
+    return (
+        <CodeConfirmationContext.Provider value={value}>
+            {children}
+        </CodeConfirmationContext.Provider>
+    );
+}
