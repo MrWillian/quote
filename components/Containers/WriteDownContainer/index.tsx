d228280f47af9536fe871ee707b9f73b9f26e62d
MrWillian <willian.dev10@gmail.com>
2023-02-23 16:06:57 -0300

    fix(components): set image path

--- components/Containers/WriteDownContainer/index.tsx
+++ components/Containers/WriteDownContainer/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import art from '../../public/static/images/artwrite_down_quote_app.png';
+import art from '../../../public/static/images/artwrite_down_quote_app.png';
 
 export const WriteDownContainer = () => (
     <section className="flex h-full w-1/2 justify-center items-center p-4">
