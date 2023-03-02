fcf7050fc51ebf0f93c76c24754ec605319cc0d9
MrWillian <willian.dev10@gmail.com>
2023-03-02 15:03:47 -0300

    style(lib): add breakline

--- lib/listQuotes.ts
+++ lib/listQuotes.ts
@@ -6,7 +6,8 @@ export async function getQuotesList(user_id: string) {
         await fetch(`https://1ruz0p0nia.execute-api.us-east-1.amazonaws.com/quotes?user_id=${user_id}`, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
-        }).then((response) => response.json())
+        })
+        .then((response) => response.json())
         .then((fetchData) => {
             status = '200';
             data = fetchData.Items;
