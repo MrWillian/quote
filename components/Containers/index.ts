f753cc23f563a30c38f1d9f778d391b40af650e7
MrWillian <willian.dev10@gmail.com>
2023-03-02 15:05:15 -0300

    refactor(components): export QuoteRegister

--- components/Containers/index.ts
+++ components/Containers/index.ts
@@ -1,5 +1,6 @@
 import { WriteDownContainer } from './WriteDownContainer';
 import { ConfirmationContainer } from './ConfirmationContainer';
 import { QuotesContainer } from './QuotesContainer';
+import { QuoteRegisterContainer } from './QuoteRegisterContainer';
 
-export { WriteDownContainer, ConfirmationContainer, QuotesContainer }
+export { WriteDownContainer, ConfirmationContainer, QuotesContainer, QuoteRegisterContainer }
