82b025a5c1827abe712c51af064ef3ef32e3d8bb
MrWillian <willian.dev10@gmail.com>
2023-03-03 11:11:00 -0300

    refactor(dashboard): extract DashboardHeader

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,30 +1,15 @@
-import { useEffect, useState } from 'react';
-import { DashboardLayout, QuoteRegisterContainer, QuotesContainer, SearchButton } from '../../components';
-import { useAuth } from "../../contexts/AuthContext";
+import { useState } from 'react';
+import { DashboardHeader, DashboardLayout, QuoteRegisterContainer, QuotesContainer, SearchButton } from '../../components';
 import { useDebounce } from '../../hooks/useDebounce';
 
 export default function Home() {
-    const [name, setName] = useState('');
-    const { getName } = useAuth();
     const [filter, setFilter] = useState('');
     const debouncedFilter = useDebounce(filter, 500);
-
-    useEffect(() => {
-        handleName();
-    }, []);
-
-    const handleName = async () => {
-        const name = await getName();
-        setName(name);
-    }
 
     return (
         <DashboardLayout>
             <div className="flex flex-col p-8 w-screen">
-                <div>
-                    <h1 className="uppercase font-bold tracking-wide text-2xl">Hello, <span className="text-accent-color tracking-wider">{name}</span></h1>
-                    <h2 className="text-accent-color font-bold text-lg">Veja todas as suas memórias...</h2>
-                </div>
+                <DashboardHeader />
                 <div className="flex">
                     <div className='w-3/4 md:w-2/4 h-2/4'>
                         <div className="flex items-center justify-center my-6 w-full">
