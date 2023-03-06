5673327dd9fb412312fcbf74dba4e33e07d61c8a
MrWillian <willian.dev10@gmail.com>
2023-03-06 16:30:05 -0300

    refactor(dashboard): change height

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <div className="flex flex-col p-8 w-screen">
                 <DashboardHeader />
                 <div className="flex">
-                    <div className='w-3/4 md:w-2/4 h-2/4'>
+                    <div className='w-3/4 md:w-2/4 h-3/4'>
                         <div className="flex items-center justify-center my-6 w-full">
                             <form method="GET" className="w-full">
                                 <div className="relative text-gray-600 focus-within:text-gray-400">
