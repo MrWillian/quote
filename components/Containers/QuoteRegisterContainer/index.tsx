9c1ca02f5dd28cabe80e3804659abe2aeceb26b6
MrWillian <willian.dev10@gmail.com>
2023-03-06 10:56:32 -0300

    refactor(QuoteRegister): implement animation on hit register button

--- components/Containers/QuoteRegisterContainer/index.tsx
+++ components/Containers/QuoteRegisterContainer/index.tsx
@@ -15,7 +15,7 @@ let schema = object().shape({
 
 export const QuoteRegisterContainer = () => {
     const { getSub } = useAuth();
-    const { register, handleSubmit, formState: { errors }, reset } = useForm({ 
+    const { register, handleSubmit, formState: { isSubmitting, errors }, reset } = useForm({ 
         mode: 'all', 
         resolver: yupResolver(schema),
         defaultValues: {
@@ -76,9 +76,19 @@ export const QuoteRegisterContainer = () => {
                     <div className="flex w-full justify-center items-center">
                         <button 
                             type="submit"
-                            className="mt-6 z-90 bg-primary-color w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-violet-900 hover:drop-shadow-2xl hover:animate-bounce"
+                            className="mt-6 z-90 bg-primary-color w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:drop-shadow-2xl hover:animate-bounce"
+                            disabled={isSubmitting}
                         >
-                            <span className="w-20">&#43;</span>
+                            {!isSubmitting ? 
+                                    <span className="w-20">&#43;</span>
+                                :
+                                    <svg className="animate-spin h-10 w-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4335 4335" width="100" height="100">
+                                        <path 
+                                            fill="#5C5091" 
+                                            d="M3346 1077c41,0 75,34 75,75 0,41 -34,75 -75,75 -41,0 -75,-34 -75,-75 0,-41 34,-75 75,-75zm-1198 -824c193,0 349,156 349,349 0,193 -156,349 -349,349 -193,0 -349,-156 -349,-349 0,-193 156,-349 349,-349zm-1116 546c151,0 274,123 274,274 0,151 -123,274 -274,274 -151,0 -274,-123 -274,-274 0,-151 123,-274 274,-274zm-500 1189c134,0 243,109 243,243 0,134 -109,243 -243,243 -134,0 -243,-109 -243,-243 0,-134 109,-243 243,-243zm500 1223c121,0 218,98 218,218 0,121 -98,218 -218,218 -121,0 -218,-98 -218,-218 0,-121 98,-218 218,-218zm1116 434c110,0 200,89 200,200 0,110 -89,200 -200,200 -110,0 -200,-89 -200,-200 0,-110 89,-200 200,-200zm1145 -434c81,0 147,66 147,147 0,81 -66,147 -147,147 -81,0 -147,-66 -147,-147 0,-81 66,-147 147,-147zm459 -1098c65,0 119,53 119,119 0,65 -53,119 -119,119 -65,0 -119,-53 -119,-119 0,-65 53,-119 119,-119z"
+                                        />
+                                    </svg>
+                            }
                         </button>
                     </div>
                 </form>
