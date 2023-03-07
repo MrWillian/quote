446e617797065038eecbe83418a4e7ffc3f32d52
MrWillian <willian.dev10@gmail.com>
2023-03-07 16:08:19 -0300

    refactor(PasswordInput): implement rest

--- components/Inputs/PasswordInput/index.tsx
+++ components/Inputs/PasswordInput/index.tsx
@@ -1,6 +1,6 @@
 import { InputProps } from "../../../interfaces/types";
 
-export const PasswordInput = ({ value, onChange }: InputProps) => (
+export const PasswordInput = ({ value, onChange, register, ...rest }: InputProps) => (
     <div className='flex flex-col justify-center my-2'>
         <label className='text-sm font-bold' htmlFor="password">Senha</label>
         <input 
@@ -9,6 +9,7 @@ export const PasswordInput = ({ value, onChange }: InputProps) => (
             value={value}
             onChange={onChange}
             className="rounded w-full px-2 py-1 shadow-lg text-black"
+            {...rest}
         />
     </div>
 );
