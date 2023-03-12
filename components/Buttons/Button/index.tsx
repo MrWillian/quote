28fe248c513a7b0c862a5e1f5c0ac9007379c6d5
MrWillian <willian.dev10@gmail.com>
2023-03-12 15:06:11 -0300

    refactor(Button): set spinner when form is submitting

--- components/Buttons/Button/index.tsx
+++ components/Buttons/Button/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ButtonType } from '../../../interfaces/enums';
+import { SpinnerIcon } from '../../Icons';
 
 type Props = {
-  buttonType: ButtonType
+  buttonType: ButtonType,
+  isSubmitting?: boolean
 }
 
-export const Button = ({ buttonType }: Props) => {
+export const Button = ({ buttonType, isSubmitting }: Props) => {
   const accentColor = buttonType !== ButtonType.Cancel ? 'bg-accent-color' : 'bg-white';
   const fontColor = buttonType !== ButtonType.Cancel ? 'text-white' : 'text-accent-color';
 
@@ -13,10 +15,15 @@ export const Button = ({ buttonType }: Props) => {
     <button 
       type="submit"
       className={`flex justify-center items-center w-full py-2 px-4 ${accentColor} rounded shadow-lg`}
+      disabled={isSubmitting}
     >
-      <span className={`text-xl tracking-widest hover:tracking-tight uppercase font-semibold ${fontColor}`}>
-        {buttonType}
-      </span>
+      {isSubmitting ?
+        <SpinnerIcon color="#FFFFFF" />
+        :
+        <span className={`text-xl tracking-widest hover:tracking-tight uppercase font-semibold ${fontColor}`}>
+          {buttonType}
+        </span>
+      }
     </button>
   );
 };
