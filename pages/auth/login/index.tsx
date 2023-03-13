e38982631f33ea8d10857e1d3d065bcaf86c61ad
MrWillian <willian.dev10@gmail.com>
2023-03-13 15:01:37 -0300

    refactor(Login): extract LoginForm

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,35 +1,7 @@
-import Link from 'next/link';
-import { FcRight } from 'react-icons/fc';
-import { 
-    AuthLayout,
-    WriteDownContainer,
-    FormHeader,
-    QuoteAppIcon,
-    EmailInput,
-    PasswordInput,
-    Button
-} from '../../../components';
-import { useFocus } from '../../../hooks/useFocus';
-import { ButtonType } from '../../../interfaces/enums';
-import { useAuth } from '../../../contexts/AuthContext';
-import { useRouter } from 'next/router';
-import { SubmitHandler } from "react-hook-form";
-import { useUserLoginForm, UserLoginProps } from '../../../hooks/useUserLoginForm';
+import { AuthLayout, WriteDownContainer, FormHeader, QuoteAppIcon } from '../../../components';
+import { LoginForm } from '../../../components/Forms';
 
-const Login = () => {    
-    const { register, handleSubmit, formState: { isSubmitting, errors } } = useUserLoginForm();
-    const router = useRouter();
-    const { login } = useAuth();
-    const [ inputRef ] = useFocus();
-
-    const onSubmit: SubmitHandler<UserLoginProps> = async ({ email, password }) => {
-        await login(email, password).then(() => {
-            router.replace('/dashboard');
-        }).catch((error) => {
-            alert(`Ocorreu algum erro... ${error.message ?? error}`);
-        });
-    }
-
+const Login = () => {
     return (
         <AuthLayout title="Login | Quote App">
             <div className="flex items-center justify-center py-4 px-2 h-screen">
@@ -40,26 +12,7 @@ const Login = () => {
                         title="Hey, hello!!" 
                         subtitle="Entre com os dados informados quando você se registrou..."
                     />
-                    <div>
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <EmailInput inputRef={inputRef} {...register('email')} error={errors.email} />
-                            <PasswordInput {...register('password')} error={errors.password} isLoginForm={true} />
-                            <div className="flex justify-between items-center mb-6">
-                                <div className="items-center flex">
-                                    <input type="checkbox" id="rememberme" name="rememberme" />
-                                    <label className='text-sm' htmlFor="rememberme">Lembrar</label>
-                                </div>
-                                <div className="items-center">
-                                    <a href="#" className='text-sm underline font-extralight'>Esqueceu a senha?</a>
-                                </div>
-                            </div>
-                            <Button buttonType={ButtonType.Login} isSubmitting={isSubmitting} />
-                            <Link href="/auth/register" className='flex items-center my-2'>
-                                <span className='text-sm underline font-extralight mr-1'>Precisa de uma conta?</span>
-                                <FcRight size={'1.5em'} color={'#282A37'} />
-                            </Link>
-                        </form>
-                    </div>
+                    <LoginForm />
                 </section>
             </div>
         </AuthLayout>
