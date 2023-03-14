d9bab0399696050d783c2bf086f06b0cebaa8560
MrWillian <willian.dev10@gmail.com>
2023-03-14 11:24:49 -0300

    refactor(layout): set main layout

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Head from 'next/head'
 
 import favicon from '/public/favicon.ico';
+import { QuoteAppIcon } from '../QuoteAppIcon';
 
 type Props = {
   children?: ReactNode
@@ -10,7 +11,7 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'Quote App' }: Props) => (
-  <div>
+  <div className="px-10">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
@@ -18,17 +19,22 @@ const Layout = ({ children, title = 'Quote App' }: Props) => (
       <link rel="shortcut icon" href={favicon.src} />
     </Head>
     <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
+      <nav className="flex items-center justify-between py-4 px-4 uppercase font-bold tracking-widest">
+        <Link href="/">
+          <QuoteAppIcon />
+        </Link>
+        <div className="flex justify-around w-1/2">
+          <Link href="/" className="hover:underline">Contact</Link>
+          <Link href="/about" className="hover:underline">FAQ</Link>
+          <div className="flex items-center justify-between gap-2">
+            <Link href="/auth/login" className="hover:underline">LOGIN</Link>
+            <span className="font-thin">{'|'}</span>
+            <a href="/auth/register" className="hover:underline">SIGN UP</a>
+          </div>
+        </div>
       </nav>
     </header>
     {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
   </div>
 )
 
