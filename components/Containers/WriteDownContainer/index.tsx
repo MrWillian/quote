6f7aeae6aa249e01196b38d950655e95d9583445
MrWillian <willian.dev10@gmail.com>
2023-03-14 15:30:34 -0300

    refactor(WriteDownContainer): set className type props

--- components/Containers/WriteDownContainer/index.tsx
+++ components/Containers/WriteDownContainer/index.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 
 import art from '../../../public/static/images/artwrite_down_quote_app.png';
 
-export const WriteDownContainer = ({ className }) => (
+type Props = {
+    className?: string
+}
+
+export const WriteDownContainer = ({ className }: Props) => (
     <section className={`flex justify-center items-center h-full w-1/2 p-4 ${className}`}>
         <Image 
             src={art} 
