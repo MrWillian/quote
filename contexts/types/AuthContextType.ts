b003c2d450ad2e083bdac7212f8697237308677b
MrWillian <willian.dev10@gmail.com>
2023-03-15 16:28:06 -0300

    feat(types): set AuthContext type

---
+++ contexts/types/AuthContextType.ts
@@ -0,0 +1,14 @@
+import { CognitoUserAttribute } from "amazon-cognito-identity-js";
+import { User } from "../../interfaces";
+
+export type AuthContextType = {
+    user: User;
+    login: (Username: string, Password: string) => Promise<unknown>;
+    logout: () => void;
+    signUp: (email: string, password: string, userAttributes: CognitoUserAttribute[]) => Promise<unknown>;
+    getSession: () => Promise<unknown>;
+    confirmCode: (email: string, confirmationCode: string) => Promise<unknown>;
+    resendConfirmationCode: (email: string) => Promise<unknown>;
+    getSub: () => Promise<string>;
+    getName: () => Promise<string>;
+};
