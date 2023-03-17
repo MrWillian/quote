4e7e549a4cfe94aa3041f3bda45f8b5accb53fb6
MrWillian <willian.dev10@gmail.com>
2023-03-17 16:19:20 -0300

    chore(app): implement translate dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { Nunito_Sans } from '@next/font/google'
 import { AuthProvider } from '../contexts/AuthContext';
 import { CodeConfirmationProvider } from '../contexts/CodeContext';
 import '../styles/index.css';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+import LanguageDetector from "i18next-browser-languagedetector";
+import translationEN from '../public/locales/en/translation.json';
+import translationPT from '../public/locales/pt/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  pt: {
+    translation: translationPT
+  }
+}
+
+i18next
+  .use(LanguageDetector)
+  .init({
+    resources,
+    fallbackLng: "pt",
+    interpolation: { escapeValue: false },  // React already does escaping
+  });
 
 const queryClient = new QueryClient();
 const nunito = Nunito_Sans({ 
@@ -17,17 +39,19 @@ const nunito = Nunito_Sans({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <CodeConfirmationProvider>
-            <main className={`${nunito.variable} font-sans`}>
-              <Component {...pageProps} />
-            </main>
-          </CodeConfirmationProvider>
-        </AuthProvider>
-      </QueryClientProvider>
-    </SessionProvider>
+    <I18nextProvider i18n={i18next}>
+      <SessionProvider session={pageProps.session}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <CodeConfirmationProvider>
+              <main className={`${nunito.variable} font-sans`}>
+                <Component {...pageProps} />
+              </main>
+            </CodeConfirmationProvider>
+          </AuthProvider>
+        </QueryClientProvider>
+      </SessionProvider>
+    </I18nextProvider>
   );
 }
 
