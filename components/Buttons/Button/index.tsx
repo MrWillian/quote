84630c842ee146df73252fc8a5a596846c0dfddb
MrWillian <willian.dev10@gmail.com>
2023-03-18 22:21:29 -0300

    feat(Button): translate buttons label

--- components/Buttons/Button/index.tsx
+++ components/Buttons/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from "react-i18next";
 import { ButtonType } from '../../../interfaces/enums';
 import { SpinnerIcon } from '../../Icons';
 
@@ -8,8 +9,22 @@ type Props = {
 }
 
 export const Button = ({ buttonType, isSubmitting }: Props) => {
+  const { t } = useTranslation();
   const accentColor = buttonType !== ButtonType.Cancel ? 'bg-accent-color' : 'bg-white';
   const fontColor = buttonType !== ButtonType.Cancel ? 'text-white' : 'text-accent-color';
+
+  const getTypeText = (type: ButtonType) => {
+    switch (type) {
+      case ButtonType.Login:
+        return t('common.button_signin');
+      case ButtonType.Register:
+        return t('common.button_signup');
+      case ButtonType.Cancel:
+        return t('common.button_cancel');
+      case ButtonType.Verify:
+        return t('common.button_verify');
+    }
+  }
 
   return (
     <button 
@@ -21,7 +36,7 @@ export const Button = ({ buttonType, isSubmitting }: Props) => {
         <SpinnerIcon color="#FFFFFF" />
         :
         <span className={`text-xl tracking-widest hover:tracking-tight uppercase font-semibold ${fontColor}`}>
-          {buttonType}
+          {getTypeText(buttonType)}
         </span>
       }
     </button>
