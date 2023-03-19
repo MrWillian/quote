87308456cb4671bef1ae3d9a96e4777f30fe7d2d
MrWillian <willian.dev10@gmail.com>
2023-03-18 22:37:34 -0300

    feat(page): translate register

--- pages/auth/register/index.tsx
+++ pages/auth/register/index.tsx
@@ -5,18 +5,23 @@ import {
     QuoteAppIcon,
     UserRegisterForm
 } from '../../../components';
+import { useTranslation } from "react-i18next";
 
-const Register = () => (
-    <AuthLayout title="Register | Quote App">
-        <div className="flex items-center justify-center py-4 px-2 h-screen">
-            <section className='flex flex-col h-full justify-center my-4 mx-10 border-r-5 border-gray-500'>
-                <QuoteAppIcon />
-                <FormHeader title="Crie sua conta!!" subtitle="Será rápido e fácil..." />
-                <UserRegisterForm />
-            </section>
-            <WriteDownContainer />
-        </div>
-    </AuthLayout>
-);
+const Register = () => {
+    const { t } = useTranslation();
+
+    return (
+        <AuthLayout title={t('register.page_title')}>
+            <div className="flex items-center justify-center h-screen px-2 py-4">
+                <section className='flex flex-col justify-center h-full mx-10 my-4 border-gray-500 border-r-5'>
+                    <QuoteAppIcon />
+                    <FormHeader title={t('register.title')} subtitle={t('register.subtitle')} />
+                    <UserRegisterForm />
+                </section>
+                <WriteDownContainer />
+            </div>
+        </AuthLayout>
+    );
+}
 
 export default Register;
