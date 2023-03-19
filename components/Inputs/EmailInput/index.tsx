a2ae7277d6f9927d234a0d20e661fb59705db173
MrWillian <willian.dev10@gmail.com>
2023-03-18 22:23:50 -0300

    feat(components): translate EmailInput

--- components/Inputs/EmailInput/index.tsx
+++ components/Inputs/EmailInput/index.tsx
@@ -1,21 +1,26 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { InputProps } from "../../../interfaces/types";
 
 export const EmailInput = React.forwardRef(
     ({ value, onChange, register, inputRef, error, ...rest }: InputProps, 
     ref: React.Ref<HTMLInputElement>
-) => (
-    <div className='flex flex-col justify-center my-2' ref={ref}>
-        <label className='text-sm font-bold' htmlFor="email">Email</label>
-        <input 
-            type="email" 
-            name="email" 
-            value={value}
-            onChange={onChange}
-            ref={inputRef} 
-            className={`rounded w-full px-2 py-1 shadow-lg text-black ${error && 'border-2 border-red-500'}`}
-            {...rest}
-        />
-        {error && <span className="font-thin text-red-500">{error.message}</span>}
-    </div>
-));
+) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className='flex flex-col justify-center my-2' ref={ref}>
+            <label className='text-sm font-bold' htmlFor="email">{t('forms.email')}</label>
+            <input 
+                type="email" 
+                name="email" 
+                value={value}
+                onChange={onChange}
+                ref={inputRef} 
+                className={`rounded w-full px-2 py-1 shadow-lg text-black ${error && 'border-2 border-red-500'}`}
+                {...rest}
+            />
+            {error && <span className="font-thin text-red-500">{error.message}</span>}
+        </div>
+    )
+});
