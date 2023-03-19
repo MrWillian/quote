1380ef8815b971374493423b41eb9dcf5601316f
MrWillian <willian.dev10@gmail.com>
2023-03-18 22:35:34 -0300

    feat(components): translate Layout

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useTranslation } from "react-i18next";
 
 import favicon from '/public/favicon.ico';
 import { QuoteAppIcon } from '../QuoteAppIcon';
@@ -11,29 +12,30 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'Quote App' }: Props) => {
+const Layout = ({ children, title }: Props) => {
   const router = useRouter();
+  const { t } = useTranslation();
 
   return (
     <div className="px-10">
       <Head>
-        <title>{title}</title>
+        <title>{title} | Quote App</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="shortcut icon" href={favicon.src} />
       </Head>
       <header>
-        <nav className="flex items-center justify-between py-4 px-4 uppercase font-bold tracking-widest">
+        <nav className="flex items-center justify-between px-4 py-4 font-bold tracking-widest uppercase">
           <Link href="/">
             <QuoteAppIcon />
           </Link>
           <div className="flex justify-around w-1/2">
-            <Link href="/contact" className={`hover:underline ${router.pathname === '/contact' && 'underline'}`}>Contact</Link>
-            <Link href="/faq" className={`hover:underline ${router.pathname === '/faq' && 'underline'}`}>FAQ</Link>
+            <Link href="/contact" className={`hover:underline ${router.pathname === '/contact' && 'underline'}`}>{t('home.contact')}</Link>
+            <Link href="/faq" className={`hover:underline ${router.pathname === '/faq' && 'underline'}`}>{t('home.faq')}</Link>
             <div className="flex items-center justify-between gap-2">
-              <Link href="/auth/login" className="hover:underline">LOGIN</Link>
+              <Link href="/auth/login" className="hover:underline">{t('common.button_signin')}</Link>
               <span className="font-thin">{'|'}</span>
-              <a href="/auth/register" className="hover:underline">SIGN UP</a>
+              <a href="/auth/register" className="hover:underline">{t('common.button_signup')}</a>
             </div>
           </div>
         </nav>
