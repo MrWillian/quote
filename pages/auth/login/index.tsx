b74ff15c431ea470cefe67e0fb3223204efde2a0
MrWillian <willian.dev10@gmail.com>
2023-03-18 22:37:28 -0300

    feat(page): translate login

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,16 +1,20 @@
+'use client'
 import { AuthLayout, WriteDownContainer, FormHeader, QuoteAppIcon } from '../../../components';
 import { LoginForm } from '../../../components/Forms';
+import { useTranslation } from "react-i18next";
 
 const Login = () => {
+    const { t } = useTranslation();
+
     return (
-        <AuthLayout title="Login | Quote App">
-            <div className="flex items-center justify-center py-4 px-2 h-screen">
+        <AuthLayout title={t('login.page_title')}>
+            <div className="flex items-center justify-center h-screen px-2 py-4">
                 <WriteDownContainer />
-                <section className='flex flex-col h-full justify-center my-4 mx-10 border-r-5 border-gray-500'>
+                <section className='flex flex-col justify-center h-full mx-10 my-4 border-gray-500 border-r-5'>
                     <QuoteAppIcon />
                     <FormHeader 
-                        title="Hey, hello!!" 
-                        subtitle="Entre com os dados informados quando você se registrou..."
+                        title={t('login.title')} 
+                        subtitle={t('login.subtitle')}
                     />
                     <LoginForm />
                 </section>
