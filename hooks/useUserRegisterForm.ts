82e288fc9f87607a14ff5480dcedeb1527f6f404
MrWillian <willian.dev10@gmail.com>
2023-03-20 16:07:51 -0300

    fix(UserRegisterForm): change translate key

--- hooks/useUserRegisterForm.ts
+++ hooks/useUserRegisterForm.ts
@@ -11,7 +11,7 @@ export type UserRegisterProps = {
 }
 
 let schema = object().shape({
-    givenName: string().required(i18n.t('forms.required_name')).min(3, i18n.t('forms.minimun_name')),
+    givenName: string().required(i18n.t('forms.required_name')).min(3, i18n.t('forms.minimum_name')),
     email: string().required(i18n.t('forms.required_email')).email(i18n.t('forms.valid_email')),
     password: string().required().matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#.\$%\^&\*])(?=.{8,})/, ""),
     confirmPassword: string().oneOf([ref('password')], i18n.t('forms.do_not_match_confirm_password')),
