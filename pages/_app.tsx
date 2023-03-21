2307087169e7f3d5c7b032ad024f11b514f1ddd6
MrWillian <willian.dev10@gmail.com>
2023-03-21 14:49:46 -0300

    refactor(app): fix detection language according navigator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,33 +2,25 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
 import { Nunito_Sans } from '@next/font/google'
 import { AuthProvider } from '../contexts/AuthContext';
 import { CodeConfirmationProvider } from '../contexts/CodeContext';
-import '../styles/index.css';
 import i18next from 'i18next';
 import { I18nextProvider } from 'react-i18next';
 import LanguageDetector from "i18next-browser-languagedetector";
-import translationEN from '../public/locales/en/translation.json';
-import translationPT from '../public/locales/pt/translation.json';
+import resources from '../public/locales/resources.js';
+import '../styles/index.css';
 
-const resources = {
-  en: {
-    translation: translationEN
-  },
-  pt: {
-    translation: translationPT
-  }
-}
+var detectionOptions = {
+  order: ["navigator", "queryString", "path", "cookie"],
+  caches: []
+};
 
-i18next
-  .use(LanguageDetector)
-  .init({
-    resources,
-    fallbackLng: "pt",
-    interpolation: { escapeValue: false },  // React already does escaping
-  });
+i18next.use(LanguageDetector).init({
+  resources,
+  detection: detectionOptions,
+  fallbackLng: "pt"
+});
 
 const queryClient = new QueryClient();
 const nunito = Nunito_Sans({ 
