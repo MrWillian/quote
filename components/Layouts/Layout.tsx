348914910a47d76a4ce766874308013f350401b6
MrWillian <willian.dev10@gmail.com>
2023-03-21 14:48:10 -0300

    refactor(layouts): extract navbar

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -1,48 +1,27 @@
 import React, { ReactNode } from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useTranslation } from "react-i18next";
-
 import favicon from '/public/favicon.ico';
-import { QuoteAppIcon } from '../QuoteAppIcon';
+import { MainBar } from '../Bars';
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title }: Props) => {
-  const router = useRouter();
-  const { t } = useTranslation();
+const Layout = ({ children, title }: Props) => (
+  <div className="px-10">
+    <Head>
+      <title>{title} | Quote App</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" href={favicon.src} />
+    </Head>
+    <header>
+      <MainBar />
+    </header>
+    {children}
+  </div>
+);
 
-  return (
-    <div className="px-10">
-      <Head>
-        <title>{title} | Quote App</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="shortcut icon" href={favicon.src} />
-      </Head>
-      <header>
-        <nav className="flex items-center justify-between px-4 py-4 font-bold tracking-widest uppercase">
-          <Link href="/">
-            <QuoteAppIcon />
-          </Link>
-          <div className="flex justify-around w-1/2">
-            <Link href="/contact" className={`hover:underline ${router.pathname === '/contact' && 'underline'}`}>{t('home.contact')}</Link>
-            <Link href="/faq" className={`hover:underline ${router.pathname === '/faq' && 'underline'}`}>{t('home.faq')}</Link>
-            <div className="flex items-center justify-between gap-2">
-              <Link href="/auth/login" className="hover:underline">{t('common.button_signin')}</Link>
-              <span className="font-thin">{'|'}</span>
-              <a href="/auth/register" className="hover:underline">{t('common.button_signup')}</a>
-            </div>
-          </div>
-        </nav>
-      </header>
-      {children}
-    </div>
-  );
-}
 
 export default Layout
