0bf89187d0bdce6621258a86fb60cbf713b7573f
MrWillian <willian.dev10@gmail.com>
2023-03-23 15:38:17 -0300

    feat(adsense); remove adsense from home layout

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -15,12 +15,6 @@ const Layout = ({ children, title }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="shortcut icon" href={favicon.src} />
-      <script 
-        async 
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2147711301878242"
-        crossOrigin="anonymous"
-      >  
-      </script>
     </Head>
     <header>
       <MainBar />
