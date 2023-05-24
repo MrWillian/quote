72463c8886fa97fcb0645ee4785dff6ad79b4608
MrWillian <willian.dev10@gmail.com>
2023-05-24 11:14:19 -0300

    refactor(login-form): handle login result

--- components/Forms/LoginForm/index.tsx
+++ components/Forms/LoginForm/index.tsx
@@ -18,29 +18,22 @@ export const LoginForm = () => {
     const { t } = useTranslation();
 
     const onSubmit: SubmitHandler<UserLoginProps> = async ({ email, password }) => {
-        await login(email, password).then((data) => {
+        const result = await login(email, password);
+        if (result.type === 'success') {
             router.replace('/dashboard');
-        }).catch((error) => {
-            if (error.code === 'UserNotConfirmedException') {
-                user.email = email;
-                resendConfirmationCode(email).then(() => {
-                    alert(t('forms.error_user_not_confirmed'));
-                    router.replace('/auth/confirm');
-                }).catch((error) => {
-                    alert(`${t('forms.error')} ${error.message ?? error}`);
-                });
-                return;
-            } else if (error.code == 'NotAuthorizedException') {
-                alert(t('forms.error_password'));
-                return;
-            } else if (error.code == 'ResourceNotFoundException') {
-                alert(t('forms.error_user_not_found'));
-                return;
-            } else if (error.code == 'PasswordResetRequiredException') {
-                // Reset Password Required
-            }
-            alert(`${t('forms.error')} ${error.message ?? error}`);
-        });
+            return;
+        }
+        if (result.error.code === 'UserNotConfirmedException') {
+            user.email = email;
+            resendConfirmationCode(email).then(() => {
+                alert(t('forms.error_user_not_confirmed'));
+                router.replace('/auth/confirm');
+            }).catch((error) => {
+                alert(`${t('forms.error')} ${error.message ?? error}`);
+            });
+            return;
+        }
+        alert(result.error.message);
     }
 
     return (
